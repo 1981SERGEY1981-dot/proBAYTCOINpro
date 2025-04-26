521f2aa82abc3bcafeaec20078c877bfdd12cf2e
1981SERGEY1981 <1981sberdser1981@gmail.com>
2025-04-26 20:43:03 +0300

    Initial commit: baytcoin

--- check_schema.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import inspect
-from models import engine  # Убедитесь, что engine импортирован из вашего модуля моделей
-
-inspector = inspect(engine)
-columns = inspector.get_columns('blocks')
-for column in columns:
-    print(column['name'], column['type'])
